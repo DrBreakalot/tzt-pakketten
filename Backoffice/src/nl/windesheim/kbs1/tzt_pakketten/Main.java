b7d2311c0986ca91010d780e002dfe2b45280183
Wilco Wolters <wilcowolters@gmail.com>
2015-03-14 12:59:16 +0100

    Add backoffice project

---
+++ Backoffice/src/nl/windesheim/kbs1/tzt_pakketten/Main.java
@@ -0,0 +1,16 @@
+package nl.windesheim.kbs1.tzt_pakketten;
+
+/**
+ *
+ * @author Wilco Wolters
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
