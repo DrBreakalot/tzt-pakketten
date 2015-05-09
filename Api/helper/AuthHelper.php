d3bfad635c2c5ce947d8eb4b245d2235d8f78470
Wilco Wolters <wilcowolters@gmail.com>
2015-05-09 15:55:57 +0200

    More utility methods, improved documentation on existing utility methods

--- Api/helper/AuthHelper.php
+++ Api/helper/AuthHelper.php
@@ -4,27 +4,42 @@ require_once 'DatabaseHelper.php';
 
 $TZT_AUTH_HEADER = "HTTP_TZT_AUTHORIZATION";
 
-$token = $_SERVER[$TZT_AUTH_HEADER];
-$user = null;
-$session = null;
-if ($token) {
-    $statement = $db->prepare('SELECT * FROM `session` WHERE `token` = :token AND `is_valid` AND `expiry_date` > NOW()');
-    $statement->execute(array(':token' => $token));
-    $session = $statement->fetch(PDO::FETCH_ASSOC);
-    if ($session) {
-        $domain = $session["domain"];
-        if ($domain === "Customer") {
-            $customerStatement = $db->prepare('SELECT * FROM `customer` WHERE `id` = :id');
-            $customerStatement->execute(array(':id' => $session["user_id"]));
-            $user = $customerStatement->fetch(PDO::FETCH_ASSOC);
-        } else if ($domain === "TrainCourier") {
-            $courierStatement = $db->prepare('SELECT * FROM `trainCourier` WHERE `id` = :id');
-            $courierStatement->execute(array(':id' => $session["user_id"]));
-            $user = $courierStatement->fetch(PDO::FETCH_ASSOC);
-        }
-        
-        if ($user) {
-            $user["type"] = $session["domain"];
+/**
+ * Fills the following variables with user data. This will not overwrite the values if a user has been found before
+ * @global string $token The token as obtained from the TZT-AUTHORIZATION header
+ * @global array $session The session obtained via the token, if available
+ * @global array $user  The user obtained via the session, if available
+ */
+function fillUserData() {
+    global $token;
+    global $user;
+    global $session;
+
+    if ($token === null) {
+        $token = $_SERVER[$TZT_AUTH_HEADER];
+        $user = null;
+        $session = null;
+
+        if ($token !== null) {
+            $statement = $db->prepare('SELECT * FROM `session` WHERE `token` = :token AND `is_valid` AND `expiry_date` > NOW()');
+            $statement->execute(array(':token' => $token));
+            $session = $statement->fetch(PDO::FETCH_ASSOC);
+            if ($session !== null) {
+                $domain = $session["domain"];
+                if ($domain === "Customer") {
+                    $customerStatement = $db->prepare('SELECT * FROM `customer` WHERE `id` = :id');
+                    $customerStatement->execute(array(':id' => $session["user_id"]));
+                    $user = $customerStatement->fetch(PDO::FETCH_ASSOC);
+                } else if ($domain === "TrainCourier") {
+                    $courierStatement = $db->prepare('SELECT * FROM `trainCourier` WHERE `id` = :id');
+                    $courierStatement->execute(array(':id' => $session["user_id"]));
+                    $user = $courierStatement->fetch(PDO::FETCH_ASSOC);
+                }
+
+                if ($user !== null) {
+                    $user["type"] = $session["domain"];
+                }
+            }
         }
     }
 }
@@ -34,12 +49,15 @@ if ($token) {
  * @param array $allowedUserTypes The required userTypes, null if no authorization required.
  * @returns bool true if successful, doesn't return otherwise
  */
-function requireUserType(array $allowedUserTypes) {
+function requireUserType(array $allowedUserTypes) {    
+    fillUserData();
+    
     global $user;
+    
     if (!$allowedUserTypes || count($allowedUserTypes) === 0) {
         return true;
     }
-    if (!$user) {
+    if ($user === null) {
         http_response_code(401);
         echo json_encode(array("error" => "Unauthorized, user not logged in"));
         die;
--- Api/helper/GeneralHelper.php
+++ Api/helper/GeneralHelper.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Fills the global variable $json with parsed json data from the post body
+ * @global type $json The data obtained by json decoding the post body
+ */
+function decodePostBody() {
+    global $json;
+    if ($json === null) {
+        $input = file_get_contents('php://input');
+        $json = json_decode($input, true);
+    }
+}
+
+/**
+ * Kills if none of the methods in $allowedMethods is found. Will return 405 to the user if this is the case
+ * @param array $allowedMethods The array of allowed methods
+ * @return true if successful, doesn't return otherwise
+ */
 function requireMethod(array $allowedMethods) {
     if (!$allowedMethods || count($allowedMethods) == 0) {
         return true;
@@ -9,11 +26,32 @@ function requireMethod(array $allowedMethods) {
     foreach ($allowedMethods as $allowedMethod) {
         if ($method === $allowedMethod) {
             $allowed = true;
+            break;
         }
     }
     if (!$allowed) {
         http_response_code(405);
         echo json_encode(array("error" => "Method not allowed", "allowed_methods" => $allowedMethods));
+        die;
+    }
+    return true;
+}
+
+/**
+ * Kills if any of the parameters in $parameters is not found in the get parameters. Returns status 400 and and error body to the user if this is the case.
+ * @param array $parameters The parameters which should all be present in the get parameters.
+ * @return boolean true if successful, doesn't return otherwise
+ */
+function requireGetParameters(array $parameters) {
+    $missingParameters = array();
+    foreach ($parameters as $parameterName) {
+        if ($_GET[$parameterName] === null) {
+            $missingParameters[] = $parameterName;
+        }
+    }
+    if (count($missingParameters) > 0) {
+        http_response_code(400);
+        echo json_encode(array("error" => "Missing parameter in URL", "parameters" => $missingParameters));
         die;
     }
     return true;
