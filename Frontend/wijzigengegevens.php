175a101d63e1210fe4301e5cd0151b0b5cf473f6
Ken de Poorter <ken.de.poorter@windesheim.nl>
2015-05-26 18:44:44 +0200

    Toevoegingen

---
+++ Frontend/wijzigengegevens.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="stijl.css" />
+        <title>Wijzigen gegevens  -  TZT</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
